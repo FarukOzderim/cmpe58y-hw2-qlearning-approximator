3268a539f3d228f27f0f5f0d96a5e6e9a0e6197a
FarukOzderim <farukozderim@gmail.com>
2020-03-09 13:33:21 +0300

    Working without moment

--- gym_cartpole_hw2_qlearning_approximation.py
+++ gym_cartpole_hw2_qlearning_approximation.py
@@ -3,32 +3,28 @@ import numpy as np
 import math
 import random
 import matplotlib.pyplot as plt
-
+import sys
 env = gym.make("CartPole-v1")
-plotData=np.zeros(1000)
+numberOfEpisodes=1000
+plotData=np.zeros(numberOfEpisodes)
 #Use the pole angle and pole angle speed data
 #Position and velocity of the cart is a little side objective, and can be solved by solving pole angle&angle velocity
 
-thetas=np.array([[np.random.random()*10,np.random.random()*10,np.random.random()*10,np.random.random()*10,np.random.random()*10],[np.random.random()*10,np.random.random()*10,np.random.random()*10,np.random.random()*10,np.random.random()*10]])
+thetas=np.array([[np.random.random()*1,np.random.random()*1,np.random.random()*1,np.random.random()*1,np.random.random()*1],[np.random.random()*1,np.random.random()*1,np.random.random()*1,np.random.random()*1,np.random.random()*1]])
 
+### TODO: try moment but dont try hard
 
 #ALPHA=0.01
 #epsilon=0.01
-gama=0.99
+gama=0.999
 
-#Epsilon starts with 1 and drops to 0.1 with time
+#Epsilon starts with 1 and drops to with time
 def chooseEpsilon(time):
 	return 0.99 ** time
-#Alpha starts with 1 and drops to 0.1 with time
-def chooseAlpha(time):
-    return 0.1
-        # return max(0.1,min(1.0,1.0- math.log10((1+time)/10)))
 
-#Normalize Pole Angle to [-pi,+pi]
-def discretizePoleAngle(angle):
-	angle= angle%np.radians(360)
-	angle=angle-np.radians(180)
-	return angle
+#Alpha is 0.01
+def chooseAlpha(time):
+    return 0.01
 
 #Choose random action with probabilty epsilon
 def chooseAction(epsilon,oldObservation):
@@ -40,6 +36,7 @@ def chooseAction(epsilon,oldObservation):
 def activationFunction(x):
 	return 1/(1+math.exp(-1*x))
 
+		
 #Update Model
 def updateModel(oldObservation,newObservation,action,reward,alpha):
 	thetasUpdate=np.array([0.0,0.0,0.0,0.0,0.0])
@@ -48,14 +45,17 @@ def updateModel(oldObservation,newObservation,action,reward,alpha):
 	for j in range(4):
 		thetasUpdate[j]+=-1*alpha*(oldObservation[j])*(oldOutput[action]-(reward+gama*np.max(newOutput)))
 	thetasUpdate[4]+=-1*alpha*(oldOutput[action]-(reward+gama*np.max(newOutput)))
-	#print(thetasUpdate)
+	
+	global thetas
 	for j in range(5):
-		thetas[action][j]+=thetasUpdate[j] 
+		thetas[action][j]+=(thetasUpdate[j])
 		if thetas[action][j]>0:
 			thetas[action][j]=min(100.0,thetas[action][j])
 		else:
-			thetas[action][j]=max(-100.0,thetas[action][j])		
-	#return thetasUpdate
+
+			thetas[action][j]=max(-100-.0,thetas[action][j])		
+
+
 #Returns output of the model
 def outputOfModel(state):
 	outputs=np.zeros(2)
@@ -65,67 +65,98 @@ def outputOfModel(state):
 	outputs[0]+=thetas[0][4]
 	outputs[1]+=thetas[1][4]
 	
-#	print(outputs)
-	
-	#outputs[0]=activationFunction(outputs[0])*10
-	#outputs[1]=activationFunction(outputs[1])*10
-
 	return outputs
 
+#State conversion is currently inactive
 def convertState(state):
-	'''	
-	state[0]=state[0]/30
-	state[1]=state[1]/20
-	state[2]=state[2]/np.radians(180)
-	state[3]=state[3]/20
-	'''
+	
+	#angle=state[2]
+	#angle= angle%np.radians(360)
+	#angle=1*(angle-np.radians(180))
+	#state[2]=angle
+	#State regularizations are closed.
+	#angle=angle-np.radians(180)
+	#state[0]=0 
+	#state[0]=state[0]/10
+	#state[1]=0 
+	#state[1]=state[1]/5
+	#state[2]=state[2]
+	#state[3]=state[3]/5
 	return state
 
 accumulatedReward=0
 #First 500 rounds, mainly for training
-for time in range(500):
+for time in range(numberOfEpisodes):
 	roundSurvival=0
 	alpha=chooseAlpha(time)
 	epsilon=chooseEpsilon(time)
 	oldObservation=env.reset()
-	oldObservation[2]=discretizePoleAngle(oldObservation[2])
 	oldObservation=convertState(oldObservation)
-	#updateTheta1=np.zeros(5)
-	#updateTheta2=np.zeros(5)
-
-#	print(thetas)
 	for survival in range(500):		
 		#env.render()
 		action=chooseAction(epsilon,oldObservation)
 		newObservation, reward, done, info = env.step(action)
 		accumulatedReward+=reward
 		roundSurvival+=reward
-		'''if done:
-			print(time, "Survival tick is:",survival, epsilon)
-			#accumulatedReward+=survival
-			break'''
-		
-		newObservation[2]=discretizePoleAngle(newObservation[2])
+
 		newObservation=convertState(newObservation)
 		updateModel(oldObservation,newObservation,action,reward,alpha)
-		'''if action==1:
-			updateTheta1+=updateModel(oldObservation,newObservation,action,reward,alpha)
-		else:
-			updateTheta2+=updateModel(oldObservation,newObservation,action,reward,alpha)
-			'''
-		#updateTheta1+=x[0]
-		#updateTheta2+=x[1]
+
 		oldObservation=newObservation
-	#thetas[0]+=updateTheta1
-	#thetas[1]+=updateTheta2
 
 	plotData[time]=roundSurvival
 	print(time, "Round reward and epsilon is:",roundSurvival, epsilon)
 
+
+env.close()
+
+#print(thetas)
+
+#print(plotData.var())
+#print(plotData.mean())
+plt.plot(plotData)
+plt.show()
+#Test part after training
+'''
 print(thetas)
 average1=accumulatedReward/500
 print("Average rewards in first 500 rounds is:",average1)
 
-env.close()
+oldObservation=env.reset()
+roundSurvival=0
+accumulatedReward=0
+for survival in range(5000):		
+	#env.render()
+	action=chooseAction(epsilon,oldObservation)
+	newObservation, reward, done, info = env.step(action)
+	accumulatedReward+=reward
+	roundSurvival+=reward
 
-print(thetas)
+	newObservation=convertState(newObservation)
+	#updateModel(oldObservation,newObservation,action,reward,alpha)
+	oldObservation=newObservation
+
+#	plotData[time]=roundSurvival
+print(time, "Round reward and epsilon is:",roundSurvival, epsilon)
+print(roundSurvival," out of 5000 tick")
+
+
+oldObservation=env.reset()
+roundSurvival=0
+accumulatedReward=0
+for survival in range(1000):		
+	env.render()
+	action=chooseAction(epsilon,oldObservation)
+	newObservation, reward, done, info = env.step(action)
+	accumulatedReward+=reward
+	roundSurvival+=reward
+
+	newObservation=convertState(newObservation)
+	#updateModel(oldObservation,newObservation,action,reward,alpha)
+	oldObservation=newObservation
+
+#	plotData[time]=roundSurvival
+print(time, "Round reward and epsilon is:",roundSurvival, epsilon)
+
+'''
+
